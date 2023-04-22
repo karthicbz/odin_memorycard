655010f9b86289b0e2f77818bcf6611ecf8a1427
Karthick <pkarthicbz@live.in>
2023-04-22 19:10:55 +0530

    display character image

--- src/components/gameBoard.js
+++ src/components/gameBoard.js
@@ -8,6 +8,7 @@ const fetchCharacters = async ()=>{
 
 export default function GameBoard(){
     const [charactersData, setCharactersData] = useState([]);
+    const [characterPoints, setCharacterPoints] = useState([]);
 
     useEffect(()=>{
         const jsonData = fetchCharacters()
@@ -17,13 +18,18 @@ export default function GameBoard(){
         }).catch(err=>{
             console.log(err);
         })
+
+        charactersData.slice(0,5).map(data=>{
+            setCharacterPoints([...characterPoints, {name:data['name'], isClicked:false}]);
+        })
+
     }, []);
 
     return(
         <div className="game-board">
             {charactersData.slice(0,5).map(data=>{
                 return <div className="characters">
-                    <img style={{width: '200px', height: '300px'}} src={data['image']}/>
+                    <img style={{width: '200px', height: '300px'}} src={data['image']} alt="hp-character"/>
                     <p>{data['name']}</p>
                     </div>
             })}
